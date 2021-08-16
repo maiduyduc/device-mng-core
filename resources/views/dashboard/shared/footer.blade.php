3e26b1dfd6c8903f563b248a499a69c2e9a9d7b5
Mai Duy Duc <aiyeutoiko07@gmail.com>
2021-08-16 17:30:14 +0700

    fix: Misspelled Controller Name

--- resources/views/dashboard/shared/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<footer class="c-footer">
-  <div><a href="https://www.hactech.edu.vn/" target="_blank" >Trường Cao đẳng nghề Bách Khoa Hà Nội</a> &copy; 2021</div>
-  <div class="ml-auto">Đồ án tốt nghiệp</div>
-</footer>
