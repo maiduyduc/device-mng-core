0c2e2f8f4d515272e20e3b4c25f8427cb931af9b
Mai Duy Duc <aiyeutoiko07@gmail.com>
2021-08-20 16:35:20 +0700

    fix room

--- app/Models/Models/apps/Room.php
+++ app/Models/Models/apps/Room.php
@@ -7,9 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
-    use HasFactory;
     protected $table = 'rooms';
-    protected $guarded = ['name', 'num_of_equip'];
+    protected $guarded = [];
 
     public function Device(){
         return $this->hasMany(Device::class, 'room_id');
