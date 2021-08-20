11991944efbf0db92183d69daea551e0ae57dc1d
Mai Duy Duc <aiyeutoiko07@gmail.com>
2021-08-20 17:08:13 +0700

    remove junk file

--- resources/assets/apps/js/roles/js/checkbox.js
+++
@@ -1,9 +0,0 @@
-$( function (){
-    $('.checkbox_wrapper').on('click', function () {
-        $(this).parents('.card2').find('.checkbox_childrent').prop('checked', $(this).prop('checked'));
-    });
-    $('.checkall').on('click', function () {
-        $(this).parents().find('.checkbox_childrent').prop('checked', $(this).prop('checked'));
-        $(this).parents().find('.checkbox_wrapper').prop('checked', $(this).prop('checked'));
-    });
-})
